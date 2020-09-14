24b3faa5bfe8adf3dc402222697e03a0c62c11d2
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-09-14 11:16:39 +0100

    Basic class comp set up with state

---
+++ src/Chameleon.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Chameleon extends Component {
+  constructor() {
+    super();
+    this.state = {
+      colour: "black",
+      buttons: ["red", "blue", "yellow"],
+    };
+  }
+  render() {
+    const buttons = this.state.buttons.map((button, i) => {
+      return <Button colour={button} key={i} />;
+    });
+    return (
+      <div>
+        <h1>I am a Chameleon</h1>
+        {buttons}
+      </div>
+    );
+  }
+}
+
+export default Chameleon;
